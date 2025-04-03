ee13276422303a88b2ffe0f5c1b9a4d6887d2333
ppsahu <pratyushpsahu2@gmail.com>
2025-04-03 03:52:28 -0700

    updated README

--- cypress/e2e/test1VisitWebpage.cy.js
+++ cypress/e2e/test1VisitWebpage.cy.js
@@ -1,5 +1,5 @@
 describe('First Test', () => {
   it('passes', () => {
-    cy.visit('https://example.cypress.io') // Visit Linq's test webpage
+    cy.visit('https://staging-web.linqapp.com/welcome') // Visit Linq's test webpage
   })
 })
